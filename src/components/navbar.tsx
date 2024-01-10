939638d77188c262caa9152fed6a91b2f5870d19
Tanish Makadia <tanishmakadia@gmail.com>
2024-01-09 23:27:25 -0500

    feat: Add dividing line

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,8 +2,8 @@ import TMLogo from "./tanish-makadia-logo";
 
 export default function NavBar() {
   return (
-    <nav className="flex min-w-full h-50px bg-grey-light/5 border-b border-grey-border">
-      <div className="flex min-w-40 justify-center items-center group">
+    <nav className="flex min-w-full h-50px bg-grey-light/5 border-b border-grey-border items-center">
+      <div className="flex min-w-36 justify-center items-center group">
         <TMLogo
           color="#8B949E"
           hoverColor="#C9D1D9"
@@ -11,10 +11,11 @@ export default function NavBar() {
           height={45}
           className="transition-colors duration-100 ease-out group-hover:fill-[var(--svg-hover-color)]"
         />
-        <text className="font-bold text-grey-medium group-hover:text-grey-light transition-colors duration-100 ease-out">
+        <text className="font-bold text-grey-medium group-hover:text-grey-light transition-colors duration-100 ease-out pr-3">
           TM
         </text>
       </div>
+      <div className="w-px h-25px border-r border-grey-border" />
     </nav>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,38 +1,39 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
-        'blue-bg-dark': '#0D1117',
-        'blue-bg-light': '#161B22',
-        'red-bg-dark': '#221616',
-        'grey-light': '#C9D1D9',
-        'grey-medium': '#8B949E',
-        'grey-dark': '#6E7681',
-        'grey-border': '#30363D',
-        'blue-accent': '#388BFD',
-        'blue-link': '#58A6FF',
-        'blue-light': '#79C0FF',
-        'blue-lightest': '#A5D6FF',
-        'red-accent': '#D47616',
+        "blue-bg-dark": "#0D1117",
+        "blue-bg-light": "#161B22",
+        "red-bg-dark": "#221616",
+        "grey-light": "#C9D1D9",
+        "grey-medium": "#8B949E",
+        "grey-dark": "#6E7681",
+        "grey-border": "#30363D",
+        "blue-accent": "#388BFD",
+        "blue-link": "#58A6FF",
+        "blue-light": "#79C0FF",
+        "blue-lightest": "#A5D6FF",
+        "red-accent": "#D47616",
       },
       height: {
-        '50px': '50px',
-        '150px': '150px',
+        "25px": "25px",
+        "50px": "50px",
+        "150px": "150px",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
