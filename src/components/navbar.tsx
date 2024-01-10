5712c2caa46e76d1352f889fdbb2c6b180b053e1
Tanish Makadia <tanishmakadia@gmail.com>
2024-01-09 22:58:09 -0500

    feat: Adjust colors of logo in navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,7 +4,7 @@ export default function NavBar() {
   return (
     <nav className="flex min-w-full h-50px bg-grey-light/5 border-b border-grey-border">
       <div className="flex min-w-40 justify-center items-center">
-        <TMLogo color="#FFFFFF" hoverColor="#111111" width={45} height={45} />
+        <TMLogo color="#8B949E" hoverColor="#C9D1D9" width={45} height={45} />
         <text className="font-bold text-grey-medium">TM</text>
       </div>
     </nav>
