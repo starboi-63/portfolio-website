d941b07e3fa7f4c92a727b66ef45fb0e8e8b189f
Tanish Makadia <tanishmakadia@gmail.com>
2024-01-16 15:57:23 -0500

    feat: Add boilerplate for footer component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import NavBar from "@/components/navbar";
 import "./globals.css";
+import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,8 +14,11 @@ export default function RootLayout(props: RootLayoutProps) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NavBar />
-        {props.children}
+        <div id="page-container" className="relative min-h-screen">
+          <NavBar />
+          {props.children}
+          <Footer />
+        </div>
       </body>
     </html>
   );
---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="absolute bottom-0 w-screen h-14 bg-slate-400/5 z-10">
+      <span>Footer</span>
+    </footer>
+  );
+}
